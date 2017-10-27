aba64518721a96298a644e1b3f6959e99c1786ca
MrF3lix <felix.saaro@me.com>
2017-10-27 14:52:31 +0200

    Introduce animations; Cleanup;
    Introduction of animation while updating the position of the rows.

--- js/rating.js
+++ js/rating.js
@@ -2,23 +2,7 @@ $(document).ready(function() {
     populateTable();
 
     $(".vote").on('click', function(e){
-        var isUpvote = false;
-        if($(this).hasClass("up-vote"))
-        {
-            isUpvote = true;
-        }
-        
-        var index = getIndexOfClickedItem(e);
-        var oldIndex;
-
-        updateVoteCount(index, isUpvote);
-
-        while(index != oldIndex)
-        {
-            updatePositionOfElement(index, isUpvote);
-            oldIndex = index;            
-            index = getIndexOfClickedItem(e);
-        }
+        updateTableSort(e);        
     });
 });
 
@@ -27,71 +11,165 @@ function populateTable()
     addItemToTable("1", "500 Startups", 0);
     addItemToTable("2", "NEA", 0);
     addItemToTable("3", "Y Combinator", 0);
+    addItemToTable("4", "Sequoia Capital", 0);
+    addItemToTable("5", "Accel", 0);
+    addItemToTable("6", "Kleiner Perkins", 0);
+    addItemToTable("7", "Intel Capital", 0);
+    addItemToTable("8", "Greylock Partners", 0);
 }
 
 function addItemToTable(index, name, votes)
 {
-    $("#companies tbody").append('<tr id="'+index+'"><td>' + index + '</td><td>' + name + '</td><td class="votes">' + votes + '</td><td><button type="button" class="btn btn-success vote up-vote">UP</button><button type="button" class="btn btn-danger vote down-vote">DOWN</button></td></tr>');
+    var row = $('<tr id="'+index+'">' +
+                    '<td>' + index + '</td>' +
+                    '<td>' + name + '</td>' +
+                    '<td class="votes">' + votes + '</td>' +
+                    '<td>' +
+                        '<button type="button" class="btn btn-success vote up-vote">UP</button>' +
+                        '<button type="button" class="btn btn-danger vote down-vote">DOWN</button>' +
+                    '</td>' +
+                '</tr>');
+
+    $("#companies tbody").append(row);
+    $("#" + index).children("td").wrapInner("<div class='cell-content'></div>");
 }
 
-function getIndexOfClickedItem(e)
+function updateTableSort(e)
 {
-    return $(e.target.closest("tr")).index() + 1;
+    var isUpvote = false;
+    var index = getIndexOfClickedItem(e);
+    var currentRow = getElementFromIndex(index);
+
+    if($(e.target).hasClass("up-vote"))
+    {
+        isUpvote = true;
+    }
+    
+    updateVoteCount(currentRow, index, isUpvote);
+    updatePositionOfElement(currentRow, index, isUpvote);
 }
 
-function updateVoteCount(index, isUpvote)
+function updateVoteCount(currentRow, index, isUpvote)
 {
-    var row = getElementFromIndex(parseInt(index));
-
-    var voteDisplayElement = row.children(".votes:first");
-    var currentVoteValueText = voteDisplayElement.text();
-    var currentVoteValue = parseInt(currentVoteValueText);
+    var voteCount = getVoteCountFromIndex(index);
+    var newVoteCount = voteCount;
 
     if(isUpvote)
     {
-        voteDisplayElement.text(currentVoteValue + 1);   
+        newVoteCount++; 
     }
     else
     {        
-        voteDisplayElement.text(currentVoteValue - 1);   
+        newVoteCount--;
     }
-
+    
+    currentRow.find(".votes>div:first").text(newVoteCount); 
 }
 
-function updatePositionOfElement(indexText, isUpvote)
+function updatePositionOfElement(currentElement, index, isUpvote)
 {
-    var index = parseInt(indexText);
-    var nextElement = getNextElement(index, isUpvote);
-    var currentElement = getElementFromIndex(index);
-    var nextElementVoteCount = parseInt($(nextElement.children(".votes:first")).text());
-    var currentElementVoteCount = parseInt($(currentElement.children(".votes:first")).text());
+    var nextIndex = getNextElementIndex(index, isUpvote);
+    var nextElement = getElementFromIndex(nextIndex);
+    var currentElementVoteCount = getVoteCountFromIndex(index);
+    var nextElementVoteCount = getVoteCountFromIndex(nextIndex);
+    var currentElementCellContent = currentElement.find(".cell-content");
+    var nextElementCellContent = nextElement.find(".cell-content");
+    var dif = index - nextIndex;
 
     if(!isUpvote && nextElementVoteCount > currentElementVoteCount)
-    {
-        currentElement.insertAfter(currentElement.next());
+    {    
+        var slideDistance = currentElement.height() * -dif; 
+        var animationSpeed = 100 * -dif;
+
+        slideRowDown(animationSpeed, currentElementCellContent, currentElement, nextElement, slideDistance)
+        slideNextElement(animationSpeed, nextElementCellContent, -slideDistance)
     }
 
     if(isUpvote && nextElementVoteCount < currentElementVoteCount)
     {
-        currentElement.insertBefore(currentElement.prev());
+        var slideDistance = currentElement.height() * dif; 
+        var animationSpeed = 100 * dif;
+
+        slideRowUp(animationSpeed, currentElementCellContent, currentElement, nextElement, -slideDistance)
+        slideNextElement(animationSpeed, nextElementCellContent, slideDistance)
     }
 }
 
-function getNextElement(index, isUpvote)
+function slideRowDown(animationSpeed, cellContent, currentElement, nextElement, currentElementSlideDistance)
+{ 
+        cellContent.animate({
+            top: currentElementSlideDistance
+        }, animationSpeed).promise().then(function(){
+            currentElement.insertAfter(nextElement);
+            cellContent.css('top', '0');
+        });
+}
+
+function slideRowUp(animationSpeed, cellContent, currentElement, nextElement, currentElementSlideDistance)
+{ 
+        cellContent.animate({
+            top: currentElementSlideDistance
+        }, animationSpeed).promise().then(function(){
+            currentElement.insertBefore(nextElement);
+            cellContent.css('top', '0');
+        });
+}
+
+function slideNextElement(animationSpeed, nextCellContent, nextElementSlideDistance)
 {
+    nextCellContent.animate({
+        top: nextElementSlideDistance
+    }, animationSpeed).promise().then(function(){
+        nextCellContent.css('top', '0');
+    });
+}
+
+function getNextElementIndex(index, isUpvote)
+{
+    var nextIndex = index;  
+    
     if(isUpvote)
-    {
-        index--;
+    {   
+        do{
+            nextIndex--;
+
+            var curVote = getVoteCountFromIndex(index);
+            var nextVote = getVoteCountFromIndex(nextIndex)
+
+        }while(curVote > nextVote || nextVote == null);
+
+        nextIndex++;
     }
     else
     {
-        index++;
+        do{
+            nextIndex++;
+
+            var curVote = getVoteCountFromIndex(index);
+            var nextVote = getVoteCountFromIndex(nextIndex)
+
+        }while(curVote < nextVote || nextVote == null);
+
+        nextIndex--;
     }
-    
-    return getElementFromIndex(index);
+
+    return nextIndex;
 }
 
 function getElementFromIndex(index)
 {
     return $("#companies tr").eq(index);
+}
+
+function getVoteCountFromIndex(index)
+{
+    var row = getElementFromIndex(index);
+    var countText = row.find(".votes:first").text();
+
+    return parseInt(countText);
+}
+
+function getIndexOfClickedItem(e)
+{
+    return $(e.target.closest("tr")).index() + 1;
 }
